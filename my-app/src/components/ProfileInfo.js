31594d25351cbcf5764f18fcbc5a536500f27391
Matt T <matt.thurmond@gmail.com>
2020-01-04 15:00:46 -0500

    format birthday

--- my-app/src/components/ProfileInfo.js
+++ my-app/src/components/ProfileInfo.js
@@ -25,8 +25,9 @@ class ProfileInfo extends React.Component {
 
     render() {
         let userIndex = 0
+        
+        //format marketing emails boolean
         let marketingEmails = ''
-        console.log(this.state.items)
         if (this.state.items[userIndex].marketing_emails) {
             marketingEmails = "On"
         }
@@ -34,17 +35,21 @@ class ProfileInfo extends React.Component {
             marketingEmails = "Off"
         }
 
+        //format birthday
+        let unformattedBirthday = new Date(this.state.items[userIndex].birthday)
+        const options = {month: 'short', day: 'numeric', year: 'numeric'}
+        let formattedBirthday = unformattedBirthday.toLocaleDateString('en-US', options)
 
     return (
         //if always in profile page, can put it there. only turn into a component if you think you'll use more than once. 
         //could make an "InfoList" component, not hard coded for profile page. Pass data structure that can be iterated through, then sub-component ("InfoListItem) that can specify key/value pairs. Pages will act as containers, i.e. smart components, that are responsible for data manipulation. Other components, like "InfoListItem", are 'dumb' components. 
         <div>
-            <h1 className="mb-5">{this.state.items[0].first} {this.state.items[userIndex].last}</h1>
+            <h1 className="mb-5">{this.state.items[userIndex].first} {this.state.items[userIndex].last}</h1>
 
             <div className="font-weight-light">Email address</div>
             <h5 className="profile-value mb-4">{this.state.items[userIndex].email}</h5>
             <div className="font-weight-light">Birthday</div>
-            <h5 className="profile-value mb-4">{this.state.items[userIndex].birthday}</h5>
+            <h5 className="profile-value mb-4">{formattedBirthday}</h5>
             <div className="font-weight-light">Marketing emails</div>
             <h5 className="profile-value mb-4">{marketingEmails}</h5>
 
